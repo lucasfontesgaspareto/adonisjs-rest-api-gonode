a2896ce58852b53ad827c394794ff50a4cacc5db
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2020-05-09 18:45:51 -0300

    Upload de arquivos

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,123 @@
+'use strict'
+
+const Model = use('App/Models/File')
+const Helpers = use('Helpers')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with files
+ */
+class FileController {
+  /**
+   * Show a list of all files.
+   * GET files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new file.
+   * GET files/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new file.
+   * POST files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '4mb' })
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.moved()) {
+        throw upload.error()
+      }
+
+      const file = await Model.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (error) {
+      return response
+        .status(error.status)
+        .send({ error: { message: 'Erro no upload de arquivo' } })
+    }
+  }
+
+  /**
+   * Display a single file.
+   * GET files/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing file.
+   * GET files/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update file details.
+   * PUT or PATCH files/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a file with id.
+   * DELETE files/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = FileController
--- start/routes.js
+++ start/routes.js
@@ -17,6 +17,10 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+
 Route.post('sessions', 'SessionController.store')
+
 Route.post('forgot-password', 'ForgotPasswordController.store')
 Route.put('forgot-password', 'ForgotPasswordController.update')
+
+Route.post('files', 'FileController.store')
