82325c637b2753101e874f1db72a0899bf49aa6d
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2020-05-10 14:04:43 -0300

    Internacionalizacao

--- app/Validators/ForgorPassword.js
+++ app/Validators/ForgorPassword.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class ForgorPassword {
   get rules () {
     return {
       email: 'required|email',
       redirect_url: 'required|url'
     }
+  }
+
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
--- app/Validators/Session.js
+++ app/Validators/Session.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class Session {
   get rules () {
     return {
       email: 'required|email',
       password: 'required'
     }
+  }
+
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
