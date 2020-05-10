82a314d64f4bce8a883f62f0e52aa7aaaff4b3a2
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2020-05-10 15:39:27 -0300

    Fila com Redis

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Helpers = use('Helpers')
-const Mail = use('Mail')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 const TaskHook = exports = module.exports = {}
 
 TaskHook.sendNewTaskMail = async (taskInstance) => {
@@ -13,24 +13,5 @@ TaskHook.sendNewTaskMail = async (taskInstance) => {
   const file = await taskInstance.file().fetch()
   const { title } = taskInstance
 
-  await Mail.send(
-    ['emails.new_task'],
-    {
-      username,
-      title,
-      hasAttachment: !!file
-    },
-    message => {
-      message
-        .to(email)
-        .from('lucas@rocketseat.com.br', 'Lucas | Rocketseat')
-        .subject('Nova tarefa para você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, title, file }, { attempts: 3 })
 }
