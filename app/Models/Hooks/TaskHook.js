1ae73410b295252e7e956235b188fa7eb3b4c387
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2020-05-10 14:39:47 -0300

    Criando hook de tarefa

---
+++ app/Models/Hooks/TaskHook.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const Helpers = use('Helpers')
+const Mail = use('Mail')
+const TaskHook = exports = module.exports = {}
+
+TaskHook.sendNewTaskMail = async (taskInstance) => {
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) {
+    return false
+  }
+
+  const { email, username } = await taskInstance.user().fetch()
+  const file = await taskInstance.file().fetch()
+  const { title } = taskInstance
+
+  await Mail.send(
+    ['emails.new_task'],
+    {
+      username,
+      title,
+      hasAttachment: !!file
+    },
+    message => {
+      message
+        .to(email)
+        .from('lucas@rocketseat.com.br', 'Lucas | Rocketseat')
+        .subject('Nova tarefa para você')
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        })
+      }
+    }
+  )
+}
